fa05078d8af0c7147283a50d58af3d1f2d015c55
Suni Kumar <suni.kumar@qualibar.com>
2024-05-31 11:25:17 +0530

    directory creation and deletion

--- dir.js
+++ dir.js
@@ -1,2 +1,30 @@
+// this module is regarding creating directory 
+
 const fs = require('fs');
+
+// to create a directory
+fs.mkdir('./new', err => {
+    if(err) throw err;
+    console.log('New directory created');
+});
+
+// checking if directory exists already, create only if not exists
+if(!fs.existsSync('./new2')){  // it prevents the error thrown if directory already exists
+    fs.mkdir('./new2', err => {
+        if(err) throw err;
+        console.log('new2 directory created');
+    })
+}
+
+if(fs.existsSync('./new2'))
+{
+    fs.rmdir('./new2', err => {
+        if(err) throw err;
+        console.log(`new2 directory removed`);
+    })
+}
+
+
+
+
 
