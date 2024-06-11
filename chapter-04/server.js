2e96ae3edd96d2681da7b7ffde4e178b860cf27c
Suni Kumar <suni.kumar@qualibar.com>
2024-06-11 11:55:03 +0530

    adding corsoption to allow only few domains in cors

--- chapter-04/server.js
+++ chapter-04/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const PORT = process.env.PORT || 3500;
 const cors = require('cors');
 
-const {logger} = require('./middleware/logEvents');
+const { logger } = require('./middleware/logEvents');
 
 // built in middleware to handle urlencoded data
 // in other words form-data
@@ -25,7 +25,22 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.use(logger);
 
 // third party cors middleware // CROSS ORIGIN RESOURCE SHARING
-app.use(cors());
+
+// whitelisting domains limited 
+const whitelist = ["https://www.google.com", "http://localhost:5000", "http://localhost:3500"];
+
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'), false);
+        }
+    },
+    optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
 
 // regex to make the route work with only /  or only index or index.html in url
 app.get('^/$|/index(.html)?', (req, res) => {
